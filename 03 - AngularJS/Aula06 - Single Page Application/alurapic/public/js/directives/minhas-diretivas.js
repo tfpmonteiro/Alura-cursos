fab0430bb7e3ecaf06b984812b0e040afc0a5c9c
Talita <talis_phn2@hotmail.com>
2019-03-15 11:34:24 -0300

    Aula 06 - AngularJS

--- 03 - AngularJS/Aula06 - Single Page Application/alurapic/public/js/directives/minhas-diretivas.js
+++ 03 - AngularJS/Aula06 - Single Page Application/alurapic/public/js/directives/minhas-diretivas.js
@@ -1,15 +1,32 @@
 angular.module('minhasDiretivas', [])
-    .directive('meuPainel', function() {
+	.directive('meuPainel', function() {
 
-        var ddo = {};
+		var ddo = {};
 
-        ddo.restrict = 'AE';
+		ddo.restrict = "AE";
         ddo.transclude = true;
-        ddo.scope = {
+
+
+		ddo.scope = {
             titulo: '@'
         };
 
         ddo.templateUrl = 'js/directives/meu-painel.html';
 
+		return ddo;
+	})
+    .directive('minhaFoto', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
         return ddo;
-});
+    });
--- 03 - AngularJS/Aula06 - Single Page Application/alurapic/public/js/main.js
+++ 03 - AngularJS/Aula06 - Single Page Application/alurapic/public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+
+		$locationProvider.html5Mode(true);
+
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
+
+		$routeProvider.when('/fotos/cadastro-de-foto.html', {
+			templateUrl: 'partials/cadastro-de-foto.html'
+		});
+
+		$routeProvider.otherwise({redirectTo: '/fotos'});
+
+	});
